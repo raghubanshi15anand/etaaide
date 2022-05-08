7ef58a4a84997a6004f234b19c762a87b1507064
anand <raghubanshi15anand@gmail.com>
2022-05-08 13:29:09 -0700

    first commit

---
+++ src/components/pages/Information.js
@@ -0,0 +1,124 @@
+
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
+import './cssFile/Information.css';
+
+
+const Information = () => {
+
+  return (
+    <div>
+      <Card>
+        <div className='grid'>
+          <Card.Grid>
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-cogs"></i>				</span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Non Contact 					</span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  Non contact type compact vibration sensor, to detect health assets of your machine					</p>
+              </div>
+            </div>
+          </Card.Grid>
+
+          <Card.Grid >
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-laptop-code"></i>				</span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Real Time monitoring					</span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  We Prioritize hard real time. We monitor your machines real time to update any change that may have occurred.					</p>
+              </div>
+            </div>
+          </Card.Grid>
+          <Card.Grid >
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-code-branch"></i>				</span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Multiple Asset monitoring					</span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  Track multiple assets in a common place					</p>
+              </div>
+            </div>
+          </Card.Grid>
+          <Card.Grid >
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-money-check-alt"></i>				</span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Low Cost					</span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  Monitor all the sensitive information of your machines at  an minimal cost. 					</p>
+              </div>
+            </div>
+          </Card.Grid>
+          <Card.Grid >
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-brain"></i>				</span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Predictive AI					</span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  Leverage the AI algorithms to detect occurrence of error.
+                </p>
+              </div>
+            </div>
+          </Card.Grid>
+          <Card.Grid >
+            <div class="elementor-icon-box-wrapper">
+              <div class="elementor-icon-box-icon">
+                <span class="elementor-icon elementor-animation-">
+                  <i aria-hidden="true" class="fas fa-code-branch"></i>
+                </span>
+              </div>
+              <div class="elementor-icon-box-content">
+                <h4 class="elementor-icon-box-title">
+                  <span>
+                    Multiple Asset monitoring
+                  </span>
+                </h4>
+                <p class="elementor-icon-box-description">
+                  Track multiple assets in a common place
+                </p>
+              </div>
+            </div>
+          </Card.Grid>
+        </div>
+      </Card>
+
+    </div>
+  );
+}
+
+export default Information;
+
+
